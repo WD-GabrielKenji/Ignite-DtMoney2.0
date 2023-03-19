d683de9087906136903931d78da662bd69b057f2
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-03-19 00:07:36 -0300

    :wheelchair: :lipstick: feat: Deixando o botões de entrada e saida acessíveis

--- src/Components/NewTransactionModal/styles.ts
+++ src/Components/NewTransactionModal/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import * as Dialog from '@radix-ui/react-dialog';
+import * as RadioGroup from '@radix-ui/react-radio-group';
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
@@ -51,7 +52,7 @@ export const Content = styled(Dialog.Content)`
   }
 `;
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   margin-top: 0.5rem;
@@ -62,7 +63,7 @@ interface TransactionTypeButtonProps {
   variant: 'income' | 'outcome';
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButtonProps>`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -75,6 +76,17 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
   cursor: pointer;
   svg {
     color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]};
+  }
+  &[data-state='unchecked']:hover {
+    transition: background-color 0.2s;
+    background: ${props => props.theme["gray-600"]};
+  }
+  &[data-state='checked'] {
+    color: ${props => props.theme.white};
+    background: ${props => props.variant === 'income' ? props.theme["green-500"] : props.theme["red-500"]};
+    svg {
+      color: ${props => props.theme.white};
+    }
   }
 `;
 
